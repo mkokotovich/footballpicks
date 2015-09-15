f7c372e04619b955a0691b16c8d34b1810f9edc1
Matthew Kokotovich <matthew_kokotovich@symantec.com>
2015-09-15 12:11:02 -0500

    Allow user to submit again, updating prior choices

--- footballseason/views.py
+++ footballseason/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
 
 from .models import Game, Team, Pick
 
@@ -31,17 +32,28 @@ def vote(request, week_id):
 			selected_team_id=0
 
 		if (selected_team_id == game.away_team.id):
-			# Away team won, create pick and add game to it
-			pick = Pick(user_name=name, game=game, team_to_win=game.away_team, date_submitted=timezone.now())
-			pick.save()
+			# Away team chosen
+			team_selected = game.away_team
 		elif (selected_team_id == game.home_team.id):
-			# Home team won, create pick and add game to it
-			pick = Pick(user_name=name, game=game, team_to_win=game.home_team, date_submitted=timezone.now())
-			pick.save()
+			# Home team chosen
+			team_selected = game.home_team
 		elif (selected_team_id != 0):
 			# Error!
 			print("Error: Invalid selected_team_id: %d" % selected_team_id)
+			return HttpResponseRedirect(reverse('footballseason:display', args=(week_id,)))
 
+		if (selected_team_id != 0):
+			# A team was selected, first look to see if a pick already exists
+			try:
+				pick = game.pick_set.get(user_name=name)
+				# An existing pick was found, update selection
+				pick.team_to_win=team_selected
+				pick.date_submitted=timezone.now()
+			except ObjectDoesNotExist:
+				# No existing pick found, create new pick
+				pick = Pick(user_name=name, game=game, team_to_win=team_selected, date_submitted=timezone.now())
+
+			pick.save()
 
 	# Always return an HttpResponseRedirect after successfully dealing
 	# with POST data. This prevents data from being posted twice if a
