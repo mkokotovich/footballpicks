3d1dba15097ac8b746c138b75fe1763aa4b8ac12
Matthew Kokotovich <mkokotovich@gmail.com>
2015-09-25 20:48:47 -0500

    Fix URLs so APPEND_SLASH works

--- footballseason/urls.py
+++ footballseason/urls.py
@@ -6,24 +6,24 @@ urlpatterns = [
     # eg: /footballseason/
     url(r'^$', views.index, name='index'),
 
-    # eg: /footballseason/3
+    # eg: /footballseason/3/
     url(r'^(?P<week_id>[0-9]+)/$', views.display, name='display'),
 
-    # eg: /footballseason/3/submit
-    url(r'^(?P<week_id>[0-9]+)/submit$', views.submit, name='submit'),
+    # eg: /footballseason/3/submit/
+    url(r'^(?P<week_id>[0-9]+)/submit/$', views.submit, name='submit'),
 
-    # eg: /footballseason/3/vote
-    url(r'^(?P<week_id>[0-9]+)/vote$', views.vote, name='vote'),
+    # eg: /footballseason/3/vote/
+    url(r'^(?P<week_id>[0-9]+)/vote/$', views.vote, name='vote'),
 
-    # eg: /footballseason/update
-    url(r'^update$', views.update, name='update'),
+    # eg: /footballseason/update/
+    url(r'^update/$', views.update, name='update'),
 
-    # eg: /footballseason/records
-    url(r'^records$', views.records_default, name='records_default'),
+    # eg: /footballseason/records/
+    url(r'^records/$', views.records_default, name='records_default'),
 
-    # eg: /footballseason/records/2015
-    url(r'^records/(?P<season>[0-9]+)$', views.records_by_season, name='records_by_season'),
+    # eg: /footballseason/records/2015/
+    url(r'^records/(?P<season>[0-9]+)/$', views.records_by_season, name='records_by_season'),
 
-    # eg: /footballseason/records/2015/3
-    url(r'^records/(?P<season>[0-9]+)/(?P<week>[0-9]+)$', views.records_by_week, name='records_by_week'),
+    # eg: /footballseason/records/2015/3/
+    url(r'^records/(?P<season>[0-9]+)/(?P<week>[0-9]+)/$', views.records_by_week, name='records_by_week'),
 ]
