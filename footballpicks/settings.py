24aba922e30a7d333e3eca5e4697ddc323b3be31
Matthew Kokotovich <matthew_kokotovich@symantec.com>
2015-10-09 13:23:11 -0500

    Add first tests

--- footballpicks/settings.py
+++ footballpicks/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import sys
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -101,6 +102,9 @@ DATABASES = {
 }
 
 DATABASES['default'] =  dj_database_url.config(default='postgres://postgresusr:postgrespwd@localhost/fbpicks')
+#Use sqlite3 for test database
+if ('test' in sys.argv):
+    DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3'}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
---
+++ footballseason/test_models.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from .models import Team, Game, Pick, Record
+
+class TeamTests(TestCase):
+    def test_record_with_tie(self):
+        """
+        Record output should include ties
+        """
+        team = Team(wins=1, loses=2, ties=3)
+        self.assertEqual(team.record(), "(1-2-3)")
+    def test_record_with_no_ties(self):
+        """
+        Record output should not include a spot for ties
+        """
+        team = Team(wins=1, loses=2, ties=0)
+        self.assertEqual(team.record(), "(1-2)")
+
+
