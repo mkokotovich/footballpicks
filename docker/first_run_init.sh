8544bfb74c524546afca9de9e68e133cae5df9fa
Matthew Kokotovich <mjkokotovich@spscommerce.com>
2018-05-10 23:36:27 -0500

    Adds docker infrastructure for react app

--- docker/first_run_init.sh
+++ docker/first_run_init.sh
@@ -2,4 +2,4 @@
 # After the first docker-compose up, you need to run a few commands:
 docker-compose exec db /fbpicks_db/import_db.sh
 sleep 5
-docker-compose restart web
+docker-compose restart django
