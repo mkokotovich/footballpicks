51523c27f9b64b67e778e1c05a9e56630d18e9fd
Matthew Kokotovich <mjkokotovich@spscommerce.com>
2018-05-14 22:39:50 -0500

    Fixes broken auth page for old site, adds a sign out button

--- frontend/fbpicks/src/SignIn.js
+++ frontend/fbpicks/src/SignIn.js
@@ -1,21 +1,49 @@
 import React from 'react';
+import { Button } from 'antd';
 
 import SignInForm from './SignInForm'
+
+function SignOut(props) {
+  return (
+    <Button 
+      type="primary"
+      onClick={props.handleSignOut}>
+      Sign Out
+    </Button>
+  );
+}
 
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
     this.handleSignIn = this.handleSignIn.bind(this);
+    this.handleSignOut = this.handleSignOut.bind(this);
+    this.state = {
+      isSignedIn: false
+    };
   }
 
   handleSignIn(username, password) {
-    console.log(username);
-    console.log(password);
+    console.log("Signed in: " + username);
+    this.setState({isSignedIn: true});
+  }
+
+  handleSignOut() {
+    console.log("Signed out");
+    this.setState({isSignedIn: false});
   }
 
   render() {
-    return (
+    const signInOrOut = this.state.isSignedIn ? (
+      <SignOut handleSignOut={this.handleSignOut} />
+    ) : (
       <SignInForm handleSignIn={this.handleSignIn} />
+    );
+
+    return (
+      <div className="SignIn">
+        {signInOrOut}
+      </div>
     );
   }
 }
