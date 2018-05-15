d8c4dc5cce53d7b7c2734764daa2b71b5ea8fee6
Matthew Kokotovich <mjkokotovich@spscommerce.com>
2018-05-14 23:54:18 -0500

    Starts moving elements around

--- frontend/fbpicks/src/App.js
+++ frontend/fbpicks/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Row, Col } from 'antd';
 
 import GameList from './GameList'
 import SignIn from './SignIn'
@@ -6,7 +7,10 @@ import SignIn from './SignIn'
 const App = () => {
   return (
     <div className="App">
-      <SignIn/>
+      <Row type="flex" justify="space-between">
+        <Col><h2>Welcome to Football Picks</h2></Col>
+        <Col><SignIn/></Col>
+      </Row>
       <GameList season='2016' week='2'/>
     </div>
   );
