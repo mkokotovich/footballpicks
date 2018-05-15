5adb77506d6cf0ac4551eb3c552772a1c9a0aed3
Matthew Kokotovich <mjkokotovich@spscommerce.com>
2018-05-14 21:13:39 -0500

    Adds ant for ui components

--- frontend/fbpicks/src/App.js
+++ frontend/fbpicks/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Button } from 'antd';
 
 import GameList from './GameList'
 
 const App = () => {
   return (
     <div className="App">
+      <Button type="primary">Button</Button>
       <GameList season='2016' week='2'/>
     </div>
   );
