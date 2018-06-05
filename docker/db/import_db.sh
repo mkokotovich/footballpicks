bd6b7367d393a098a04af6de4e40a34a5740b0a0
Matthew Kokotovich <mjkokotovich@spscommerce.com>
2018-06-05 15:57:53 -0500

    Updates postgres to latest 9.6 image, updates db fixtures to current prod data

--- docker/db/import_db.sh
+++ docker/db/import_db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #su - postgres -c "createdb fbpicks"
-su - postgres -c "pg_restore --verbose --clean --no-acl --no-owner -h localhost -U postgres -d fbpicks /fbpicks_db/prod.dump"
+su - postgres -c "pg_restore --verbose --clean --no-acl --no-owner -h localhost -U postgres -d fbpicks /fbpicks_db/latest.dump"
